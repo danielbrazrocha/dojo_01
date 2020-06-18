d664dbaefac8e1ba8710c9f3d37c0f621ee9ed22
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-06-17 22:08:22 -0300

    initial login layout

--- sample-test/src/App.js
+++ sample-test/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
+import './styles/main.css'
 
 
 const App = () => {
--- sample-test/src/pages/login/index.js
+++ sample-test/src/pages/login/index.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAmazon, faCodepen, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+import Card from '../../components/card'
+import './login.css'
 
 const Login = () => {
-    return <h1>Sou o Login</h1>
+    return (
+        <main className="container__login">
+            <Card>
+                <h1 className="container__login-title">Sign in</h1>
+                <ul className="login__list">
+                    <li><a href="#"><FontAwesomeIcon color="black" icon={faAmazon} /></a></li>
+                    <li><a href="#"><FontAwesomeIcon color="black" icon={faGithub} /></a></li>
+                    <li><a href="#"><FontAwesomeIcon color="black" icon={faCodepen} /></a></li>
+                </ul>
+                <p>or use your email account:</p>
+
+                <input type="email" id="email" placeholder="&#xF002; Email"></input>
+                <input type="password" id="pass" placeholder="&#xF002; Password" name="password" minLength="8" required></input>
+
+                <p>Forgot your password?</p>
+
+                <button className="btn__signin">Sign in</button>
+            </Card>
+        </main>
+    )
 }
 
 export default Login
