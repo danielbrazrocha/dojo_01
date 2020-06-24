915d0933d2e083adbf4b6c7c8d2e01f4c5a06769
Vitor Braz <vitor.braz@rarolabs.com.br>
2020-06-24 20:48:34 -0300

    Alteração tela de login

--- sample-test/src/pages/login/index.js
+++ sample-test/src/pages/login/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAmazon, faCodepen, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF, faLinkedinIn, faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
 
 import Card from '../../components/card'
 import './login.css'
@@ -9,20 +9,25 @@ const Login = () => {
     return (
         <main className="container__login">
             <Card>
-                <h1 className="container__login-title">Sign in</h1>
-                <ul className="login__list">
-                    <li><a href="#"><FontAwesomeIcon color="black" icon={faAmazon} /></a></li>
-                    <li><a href="#"><FontAwesomeIcon color="black" icon={faGithub} /></a></li>
-                    <li><a href="#"><FontAwesomeIcon color="black" icon={faCodepen} /></a></li>
-                </ul>
-                <p>or use your email account:</p>
+                <div>
+                    <h1 className="container__login-title">Sign in</h1>
+                    <ul className="login__list">
+                        <li><a href="#"><FontAwesomeIcon style={{ fontSize: '22px' }} color="#34495e" icon={faFacebookF} /></a></li>
+                        <li><a href="#"><FontAwesomeIcon style={{ fontSize: '22px' }} color="#34495e" icon={faGooglePlusG} /></a></li>
+                        <li><a href="#"><FontAwesomeIcon style={{ fontSize: '22px' }} color="#34495e" icon={faLinkedinIn} /></a></li>
+                    </ul>
+                </div>
+                
+                <p className="login__list-p">or use your email account:</p>
 
-                <input type="email" id="email" placeholder="&#xF002; Email"></input>
-                <input type="password" id="pass" placeholder="&#xF002; Password" name="password" minLength="8" required></input>
+                <div className="container__login-form">
+                    <input type="email" id="email" placeholder="Email"></input>
+                    <input type="password" id="pass" placeholder="Password" name="password" minLength="8" required></input>
 
-                <p>Forgot your password?</p>
+                    <p>Forgot your password?</p>
 
-                <button className="btn__signin">Sign in</button>
+                    <button className="btn__signin">Sign in</button>
+                </div>
             </Card>
         </main>
     )
